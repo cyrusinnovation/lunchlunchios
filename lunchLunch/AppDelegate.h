299ae6db3a001cca8fb16bdc0642dce369ef2cc3
Jeff Jia <jjia@cyrusinnovation.com>
2014-04-02 14:26:20 -0400

    Initial Commit

---
+++ lunchLunch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  lunchLunch
+//
+//  Created by Cyrus on 4/2/14.
+//  Copyright (c) 2014 Cyrus Innovation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ lunchLunch/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  lunchLunch
+//
+//  Created by Cyrus on 4/2/14.
+//  Copyright (c) 2014 Cyrus Innovation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
